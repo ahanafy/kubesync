b23ad5d1e1af5d1828f261c7021e3d0880712671
Alan Hanafy <alan.hanafy@gmail.com>
2022-09-09 15:17:05 -0400

    add workflow

---
+++ scripts/manifest_updater.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+NEW_TAG="$1"
+
+# make temp directory for kustomization file work
+DIR=$(mktemp -d)
+
+# copy current release deployment yaml file into temp directory
+cp deploy/release.yaml "$DIR"/release.yaml
+
+# saves the current working directory in memory so it can be returned to at any time, and moves to the parent directory
+pushd "$DIR" || exit
+
+# list name of temp dir
+pwd
+
+# create kustomization file to dynamically replace the image:tag
+cat <<EOF >./kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+ - "release.yaml"
+images:
+ - name: ghcr.io/ahanafy/kubesync/kubesync
+   newName: ghcr.io/ahanafy/kubesync/kubesync
+   newTag: "$NEW_TAG"
+EOF
+
+
+kustomize build . > _newrelease.yaml
+
+# list files in temp dir
+ls -al
+
+# returns to the path at the top of the directory stack
+popd || exit
+
+# update release.yaml with new image:tag
+cp "$DIR"/_newrelease.yaml deploy/release.yaml
+
+# remove temp dir
+rm -rf "$DIR"
